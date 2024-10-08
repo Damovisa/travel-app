cf1763294d8c2b98e7b4a0bcee4d1319553dcd16
Damian Brady <damovisa@github.com>
2024-10-08 15:40:24 +1000

    Added points until platinum image

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,5 @@
 import StatusTable from "@/components/ui/status-table";
+import NextStatusChart from "@/app/next-status-chart";
 
 export default function Home() {
   return (
@@ -7,15 +8,18 @@ export default function Home() {
         <h1 className="text-2xl font-bold mb-4">User Profile</h1>
         <div className="flex items-center space-x-8 justify-between">
           <div>
-        <p className="text-lg"><strong>Current Status:</strong> Gold</p>
-        <p className="text-lg"><strong>Status Points:</strong> 1200</p>
-        <p className="text-lg"><strong>Points to Platinum:</strong> 300</p>
+            <p className="text-lg"><strong>Current Status:</strong> Gold</p>
+            <p className="text-lg"><strong>Status Points:</strong> 2,800</p>
+            <p className="text-lg"><strong>Points to Platinum:</strong> 1,700</p>
+            </div>
+            <div>
+            <NextStatusChart />
           </div>
           <img src="/avatar-15.png" alt="User Avatar" className="w-32 h-32 rounded-full" />
         </div>
         <div className="mt-6">
-        <h2 className="text-xl font-semibold">Points Activity</h2>
-        <StatusTable />
+          <h2 className="text-xl font-semibold">Points Activity</h2>
+          <StatusTable />
         </div>
       </div>
     </div>
